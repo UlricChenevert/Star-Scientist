40a72fbde5fe3ff36426563892056c130b20ed4e
ChenevertKnowledgeLake <william.chenevert@knowledgelake.com>
2024-06-03 08:47:56 -0500

    v 1.0.0.0

--- scripts/star_scientist_core.js
+++ scripts/star_scientist_core.js
@@ -6,16 +6,19 @@
 
 function main() {
 
-    let test_star = {
-        mass_solar_units: 0.9092, // sun masses
-        radius_solar_units: 0.8591 // sun radiuses
+    let default_star = {
+        mass_solar_units: get_input("mass-input"), // sun masses
+        radius_solar_units:	get_input("radius-input"), // sun radiuses 
     };
 
-    //console.log(test_star);
+    let basic_star_data = basic_info(default_star);
 
-    //console.log(basic_info(test_star));
+    // Display
+    round_data(basic_star_data, 3);
 
-    display_star("Yellow");
+    update_canvas('chromosphere-layer', 'clear');
+    display_star_info(basic_star_data);
+    display_star(basic_star_data);
 }
 
 // Desc: Estimates the luminosity of a main sequence star
@@ -94,25 +97,25 @@ function calculate_temperature(luminosity, radius) {
 // Pre: Temperature in Kevin
 // Post: A spectral classification (O, B, A, F, G, K, M)
 function determine_spectral_classification(temperature_kevin) {
-    if (temperature_kevin >= 3.3e4) {
+    if (temperature_kevin > 3.3e4) {
         return "O";
 
-    } else if (temperature_kevin >= 1e4) {
+    } else if (temperature_kevin >= 9.7e3) {
         return "B";
 
-    } else if (temperature_kevin >= 7.3e3) {
+    } else if (temperature_kevin >= 7.2e3) {
         return "A";
 
-    } else if (temperature_kevin >= 6e4) {
+    } else if (temperature_kevin >= 5.7e3) {
         return "F";
 
-    } else if (temperature_kevin >= 5.3e4) {
+    } else if (temperature_kevin >= 4.9e3) {
         return "G";
 
-    } else if (temperature_kevin >= 3.9e3) {
+    } else if (temperature_kevin >= 3.4e3) {
         return "K";
 
-    } else if (temperature_kevin < 2.3e3) {
+    } else if (temperature_kevin < 3.4e3) {
         return "M";
     
     } else {
@@ -170,33 +173,170 @@ function basic_info(input_star) {
     return basic_info;
 }
 
-function draw_chromosphere (brush) {
-    brush.beginPath();
-    brush.arc(250, 250, 100, 0, 2 * Math.PI);
-    brush.fill();
+// Desc: Gets input from input forms and returns the value
+function get_input(container_id) {
+    return document.getElementById(container_id).value;
 }
 
-function draw_prominence (brush) {
+// Desc: Takes a object and rounds it to significant_figures-th digit
+// Pre: an object data with objects, strings, and numbers and an int significant_figures
+// Post: All integer data is rounded
+function round_data(data, significant_figures) {
+    let round_handler = {
+        "object": () => {
+            // Recursive step
+            for (var key in data) {
+                if (!data.hasOwnProperty(key)) console.log(`Error! Round handler has unexpected key! ${key}`);
+                    
+                data[key] = round_data(data[key], significant_figures); 
+            }
+            return data;
+        }, 
+        "number": () => {
+            var data_size = Math.round(data).toString().length;
+            var data_rounded = Math.round(data/10**(data_size-significant_figures))*10**(data_size-significant_figures);
+            
+            return (data_rounded);
+        }, // Recursive break
+        "string": () => {return data;}, // Recursive break
+        "undefined": () => {return data;}, // Recursive break
+    };
 
+    return round_handler[(typeof data)]();
 }
 
-function draw_corona (brush) {
+// Desc: Slaps on a opacity to a rgb string
+// Pre: rgb string
+// Post: rgba string with alterable opacity
+function alter_opacity(color, amount) {
+    return `rgba(${color.slice(4, -1)}, ${amount})`;
+}
+
+// Converts hexadecimal color to color array
+function hexadecimal_color_to_colors (code) {
+    let hexadecimal_to_decimal = {
+        0: 0,
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        A: 10,
+        B: 11,
+        C: 12,
+        D: 13,
+        E: 14,
+        F: 15,
+    };
     
+    let decimal_numbers = []
+
+    for (let character of code) {
+        if (character == '#') continue;
+        decimal_numbers.push(hexadecimal_to_decimal[character]);
+    }
+
+    // Converts the color
+    let red = decimal_numbers[0] * 16 + decimal_numbers[1];
+    let green = decimal_numbers[2] * 16 + decimal_numbers[3];
+    let blue = decimal_numbers[4] * 16 + decimal_numbers[5];
+
+    return [red, green, blue];
 }
 
-function display_star(color) {
-    const star_container = document.getElementById("star");
+// Desc: returns an object with darker/lighter color variants
+// Pre: hexadecimal color
+// Post: object with rgb colors
+function create_color_palette(color) { 
+    let base_color = hexadecimal_color_to_colors(color);
+    
+    return {
+        base: `rgb(${base_color[0]}, ${base_color[1]}, ${base_color[2]})`,
+        
+        dark: `rgb(${Math.round(base_color[0]*0.9)}, ${Math.round(base_color[1]*0.9)}, ${Math.round(base_color[2]*0.9)})`,
+        darker: `rgb(${Math.round(base_color[0]*0.8)}, ${Math.round(base_color[1]*0.8)}, ${Math.round(base_color[2]*0.8)})`,
+        
+        light: `rgb(${Math.round(base_color[0]*1.1)}, ${Math.round(base_color[1]*1.1)}, ${Math.round(base_color[2]*1.1)})`,
+        lighter: `rgb(${Math.round(base_color[0]*1.5)}, ${Math.round(base_color[1]*1.5)}, ${Math.round(base_color[2]*1.5)})`,
+    };
+}
+
+function update_canvas (canvas_id, type) {
+    const star_container = document.getElementById(canvas_id);
+    const brush = star_container.getContext("2d");
+
+    let type_handler = {
+        clear: () => {
+            brush.fillStyle = "black";
+            brush.fillRect(0, 0, star_container.getAttribute("width"), star_container.getAttribute("height"), ); 
+            brush.stroke();
+            return;},
+    };
+
+    type_handler[type]();
+}
+
+function draw_chromosphere (brush, height, width, radius, color_palette) {
+    const shiny_size = radius*0.25;
+
+    // Creating the gradient
+    const gradient = brush.createRadialGradient(width/2, height/2, shiny_size, width/2, height/2, radius);
+    gradient.addColorStop(0, color_palette.lighter);
+    gradient.addColorStop(0.7, color_palette.light);
+    gradient.addColorStop(0.9, color_palette.base);
+    gradient.addColorStop(1, alter_opacity(color_palette.base, 0.0));
+
+    // Applying the gradient
+    brush.fillStyle = gradient;
+    brush.fillRect(0, 0, width, height);
+}
+
+function draw_prominence (brush, height, width, radius) {
+
+}
+
+function draw_corona (brush, height, width, radius, color_palette) {
+    const length = radius*2;
+
+    const gradient = brush.createRadialGradient(width/2, height/2, radius, width/2, height/2, radius+length);
+    gradient.addColorStop(0, color_palette.darker);
+    gradient.addColorStop(0.5, 'rgb(0, 0, 0, 0.3)');
+    gradient.addColorStop(1, 'rgb(0, 0, 0, 0)');
+    
+    brush.fillStyle = gradient;
+    brush.fillRect(0, 0, width, height);
+
+}
+
+function display_star(star_info) {
+    const star_container = document.getElementById("chromosphere-layer"); // FIX ME <--- Separate layers
     const brush = star_container.getContext("2d");
 
     let height = star_container.getAttribute("height");
     let width = star_container.getAttribute("width");
+    let radius = star_info.radius_solar_units * 20;
+    let color_palette = create_color_palette(star_info.color);
 
-    // Colors
-    brush.strokeStyle, brush.fillStyle = color;
-    
-    draw_corona (brush);
-    draw_prominence (brush);
-    draw_chromosphere(brush);
+    brush.beginPath();
+
+    draw_corona (brush, height, width, radius, color_palette); // Layer 2
+    draw_prominence (brush, height, width, radius); // Layer 2
+    draw_chromosphere(brush, height, width, radius, color_palette); // Layer 1
     
     brush.stroke();
+}
+
+function display_star_info(star_info) {
+    document.getElementById("metrics").innerHTML = `
+        <div>Mass: ${star_info.mass_solar_units} M<sub>&#9737</sub></div>
+        <div>Radius: ${star_info.radius_solar_units} R<sub>&#9737</sub></div>
+        <div>Luminosity: ${star_info.luminosity_solar_units} L<sub>&#9737</sub></span></div>
+        <div>Lifetime: ${star_info.lifetime_years} yr</div>
+        <div>Temperature: ${star_info.temperature_kevin} K</div>
+        <div>Spectral Classification: ${star_info.spectral_classification}</div>
+    `;
 }
--- scripts/testing_function.js
+++ scripts/testing_function.js
@@ -16,3 +16,21 @@ function function_test() {
 
     console.log("-----------------> Tests completed!");
 };
+
+function sum(data, key) {
+    let total = 0;
+    if (data[key] != undefined) {
+        return data[key]; // Break condition
+
+    } else if ((typeof data != "object")) {
+        return 0;
+
+    } else {
+        // Recursive step, gets all the sums of the child object
+        for (let child of Object.values(data)) {
+            total += sum(child, key); 
+        }
+        
+        return total;
+    }
+}
